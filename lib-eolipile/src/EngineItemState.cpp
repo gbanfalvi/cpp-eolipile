76addee42e8fa9154fdac81bb09ae87f9eaa23d0
Gabriel Banfalvi <gbanfalvi@gmail.com>
2016-02-05 10:02:43 +0100

    Pass on missing parameter

--- lib-eolipile/src/EngineItemState.cpp
+++ lib-eolipile/src/EngineItemState.cpp
@@ -4,6 +4,7 @@
 EngineItemState::EngineItemState(std::string *name, int framesPerSprite, EngineSprite n_sprites, ...) {
 	this->_stateName = name;
 	this->_sprites = std::vector<EngineSprite>();
+	this->_stateFramesPerSprite = framesPerSprite;
 }
 
 EngineItemState::~EngineItemState() {
