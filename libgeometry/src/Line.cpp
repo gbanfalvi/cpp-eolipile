6c41d87772c5e2d4735946cbdc93e734800529f8
Gabriel Banfalvi <gbanfalvi@gmail.com>
2016-03-03 10:30:41 +0100

    Move things out of header file for Line

--- libgeometry/src/Line.cpp
+++ libgeometry/src/Line.cpp
@@ -1,17 +1,11 @@
-//
-//  Line.cpp
-//  lib-geometry
-//
-//  Created by Gabriel Banfalvi on 09/02/16.
-//
-//
 
 #include "Line.hpp"
+#include "Point.hpp"
 
 Line::Line(double gradient, double intercept):_gradient(gradient), _intercept(intercept) {
     if (_gradient == 0) {
         _lineCase = Horizontal;
-        
+
     } else if (_gradient == INFINITY) {
         _lineCase = Vertical;
     }
@@ -20,19 +14,70 @@ Line::Line(double gradient, double intercept):_gradient(gradient), _intercept(in
 Line::Line(Point p1, Point p2) {
     if (p1.x == p2.x && p1.y == p2.y) {
         throw;
-        
+
     } else if (p1.x == p2.x) {
         _gradient = INFINITY;
         _intercept = p1.x;
         _lineCase = Vertical;
-        
+
     } else if (p1.y == p2.y) {
         _gradient = 0;
         _intercept = p1.y;
         _lineCase = Horizontal;
-        
+
     } else {
         _gradient = (p2.y - p1.y) / (p2.x - p1.x);
         _intercept = p2.y - _gradient*p2.x;
+    }
+}
+
+inline double Line::inverseGradient() const {
+    if (_lineCase == Horizontal) {
+        return INFINITY;
+
+    } else if (_lineCase == Vertical) {
+        return 0;
+
+    } else {
+        return -1 / _gradient;
+    }
+}
+
+inline Line Line::normalWithPoint(const Point& point) const {
+    if (_lineCase == Horizontal) {
+        return Line(INFINITY, point.x);
+
+    } else if (_lineCase == Vertical) {
+        return Line(0, point.y);
+
+    } else {
+        return Line(inverseGradient(), point.y - inverseGradient()*point.x);
+    }
+}
+
+inline Point Line::pointProjectedOntoLine(const Point& other) const {
+    if (_lineCase == Vertical) {
+        return Point(_intercept, other.y);
+
+    } else if (_lineCase == Horizontal){
+        return Point(other.x, _intercept);
+
+    } else {
+        double otherGradient = inverseGradient();
+        double otherIntercept = other.y - otherGradient*other.x;
+
+        double intersectX = (_intercept - otherIntercept)/(otherGradient - _gradient);
+        double intersectY = _gradient*intersectX + _intercept;
+
+        return Point(intersectX, intersectY);
+    }
+}
+
+inline double Line::valueOnLine(const Point& point) const {
+    Point projected = pointProjectedOntoLine(point);
+    if (_lineCase == Vertical) {
+        return projected.y;
+    } else {
+        return projected.x;
     }
 }
--- libgeometry/src/Line.hpp
+++ libgeometry/src/Line.hpp
@@ -9,74 +9,26 @@
 #ifndef Line_hpp
 #define Line_hpp
 
-
 #include <cmath>
-#include "Point.hpp"
+struct Point;
 
 struct Line {
 private:
     enum LineCase { Regular, Horizontal, Vertical};
-    
+
     LineCase _lineCase = Regular;
-    double _gradient = 0;
-    double _intercept = 0;
-    
+    double   _gradient = 0;
+    double   _intercept = 0;
+
 public:
     Line(double gradient, double intercept);
     Line(Point p1, Point p2);
     Line(const Line& line):_lineCase(line._lineCase), _gradient(line._gradient), _intercept(line._intercept){};
-    
-    inline double inverseGradient() const {
-        if (_lineCase == Horizontal) {
-            return INFINITY;
-            
-        } else if (_lineCase == Vertical) {
-            return 0;
-            
-        } else {
-            return -1 / _gradient;
-        }
-    }
-    
-    inline Line normalWithPoint(const Point& point) const {
-        if (_lineCase == Horizontal) {
-            return Line(INFINITY, point.x);
-            
-        } else if (_lineCase == Vertical) {
-            return Line(0, point.y);
-            
-        } else {
-            return Line(inverseGradient(), point.y - inverseGradient()*point.x);
-        }
-    }
-    
-    inline Point pointProjectedOntoLine(const Point& other) const {
-        if (_lineCase == Vertical) {
-            return Point(_intercept, other.y);
-            
-        } else if (_lineCase == Horizontal){
-            return Point(other.x, _intercept);
-            
-        } else {
-            double otherGradient = inverseGradient();
-            double otherIntercept = other.y - otherGradient*other.x;
-            
-            double intersectX = (_intercept - otherIntercept)/(otherGradient - _gradient);
-            double intersectY = _gradient*intersectX + _intercept;
-            
-            return Point(intersectX, intersectY);
-        }
-    }
-    
-    inline double valueOnLine(const Point& point) const {
-        Point projected = pointProjectedOntoLine(point);
-        if (_lineCase == Vertical) {
-            return projected.y;
-        } else {
-            return projected.x;
-        }
-    }
-};
 
+    double inverseGradient() const;
+    Line normalWithPoint(const Point& point) const;
+    Point pointProjectedOntoLine(const Point& other) const;
+    double valueOnLine(const Point& point) const;
+};
 
 #endif /* Line_hpp */
