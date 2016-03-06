cf54cd4052a8ce8507b47c710c1050a514416e1d
Gabriel Banfalvi <gbanfalvi@gmail.com>
2016-03-06 11:56:57 +0100

    SDLLibs gets a cleanup

---
+++ libeolipile/src/SDLLibs.hpp
@@ -0,0 +1,15 @@
+//
+//  SDLLibs.h
+//  lib-eolipile
+//
+//  Created by Gabriel Banfalvi on 10/02/16.
+//  Copyright © 2016 clueless. All rights reserved.
+//
+
+#ifndef SDLLibs_h
+#define SDLLibs_h
+
+#include <SDL2/SDL.h>
+#include <SDL2_image/SDL_image.h>
+
+#endif /* SDLLibs_h */
